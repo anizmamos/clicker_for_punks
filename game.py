3c7593b58d252ad40bad3df9a514c80216085f9c
anizmamos <57558984+anizmamos@users.noreply.github.com>
2020-03-01 13:30:53 +0400

    Add files via upload

--- game.py
+++ game.py
@@ -15,14 +15,17 @@ def game_intro(score, k):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        ekran.fill(pygame.Color("white"), (0, 0, 900, 900))
         mouse = pygame.mouse.get_pos()
         font = pygame.font.Font(None, 50)
-        text = font.render(str(score), 1, (50, 50, 100))
+        text = font.render('score: ' + str(score), 1, (50, 50, 100))
         ekran.blit(text, (50, 50))
+        text2 = font.render("Улучшение(100) ->", 1, (50, 50, 100))
+        ekran.blit(text2, (50, 100))
         if 800 > mouse[0] > 0 and 600 > mouse[1] > 500:
             pygame.draw.rect(ekran, (100, 100, 200), (0, 500, 800, 100))
             if pygame.mouse.get_pressed()[0]:
-                score += random.randint(-25, 25) * k
+                score += random.randint(-25, 25) + k
                 time.sleep(0.1)
                 ekran.fill(pygame.Color("white"), (0, 0, 300, 300))
         else:
@@ -31,11 +34,23 @@ def game_intro(score, k):
             pygame.draw.rect(ekran, (200, 100, 100), (400, 0, 500, 500))
             if pygame.mouse.get_pressed()[0]:
                 k += 1
-                score -= 100* k
+                score -= 100 * k
                 ekran.fill(pygame.Color("white"), (0, 0, 300, 300))
                 time.sleep(0.1)
         else:
             pygame.draw.rect(ekran, (100, 0, 0), (400, 0, 500, 500))
+        if score < -100:
+            ekran.fill(pygame.Color("black"), (0, 0, 900, 500))
+            text3 = font.render("Вы проиграли!", 1, (200, 0, 0))
+            ekran.blit(text3, (250, 250))
+            text4 = font.render("Заново ", 1, (200, 0, 0))
+            ekran.blit(text4, (290, 450))
+            if 800 > mouse[0] > 0 and 600 > mouse[1] > 500:
+                pygame.draw.rect(ekran, (100, 100, 200), (0, 500, 800, 100))
+                if pygame.mouse.get_pressed()[0]:
+                    score, k = 0, 0
+            else:
+                pygame.draw.rect(ekran, (0, 0, 100), (0, 500, 800, 100))
         pygame.display.update()
 
 
