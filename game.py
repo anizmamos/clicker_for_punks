b41da5ad99469f6afca5b4107faeb1bb3886056e
anizmamos <57558984+anizmamos@users.noreply.github.com>
2020-03-01 20:10:49 +0400

    Add files via upload

--- game.py
+++ game.py
@@ -1,26 +1,30 @@
 import pygame
 import random
 import time
+from os import path
 pygame.init()
 size = width, height = 800, 600
 ekran = pygame.display.set_mode(size)
+pygame.display.set_caption("Clicker for punks")
 ekran.fill((255, 255, 255))
 
 
 def game_intro(score, k):
     intro = True
-
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    score += random.randint(-25, 25) + k
         ekran.fill(pygame.Color("white"), (0, 0, 900, 900))
         mouse = pygame.mouse.get_pos()
         font = pygame.font.Font(None, 50)
         text = font.render('score: ' + str(score), 1, (50, 50, 100))
         ekran.blit(text, (50, 50))
-        text2 = font.render("Улучшение(100) ->", 1, (50, 50, 100))
+        text2 = font.render("Улучшение(" + str(100 * k) + ") ->", 1, (50, 50, 100))
         ekran.blit(text2, (50, 100))
         if 800 > mouse[0] > 0 and 600 > mouse[1] > 500:
             pygame.draw.rect(ekran, (100, 100, 200), (0, 500, 800, 100))
@@ -39,7 +43,8 @@ def game_intro(score, k):
                 time.sleep(0.1)
         else:
             pygame.draw.rect(ekran, (100, 0, 0), (400, 0, 500, 500))
-        if score < -100:
+
+        if score < -500:
             ekran.fill(pygame.Color("black"), (0, 0, 900, 500))
             text3 = font.render("Вы проиграли!", 1, (200, 0, 0))
             ekran.blit(text3, (250, 250))
@@ -49,11 +54,9 @@ def game_intro(score, k):
                 pygame.draw.rect(ekran, (100, 100, 200), (0, 500, 800, 100))
                 if pygame.mouse.get_pressed()[0]:
                     score, k = 0, 0
-            else:
-                pygame.draw.rect(ekran, (0, 0, 100), (0, 500, 800, 100))
         pygame.display.update()
 
 
-game_intro(0, 1)
+game_intro(0, 0)
 pygame.display.flip()
 pygame.quit()
